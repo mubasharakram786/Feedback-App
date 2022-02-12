e5e17a9c9a82163ec9b48d58d15cb9aabc0986ff
makram786 <makram.dev@pk.see.biz>
2022-02-12 11:15:48 +0500

    wip: react journey

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Mubashar', age: 26 },
+      { name: 'Amir', age: 25 },
+      { name: 'Babar', age: 24 },
+      { name: 'Umar', age: 24 }
+    ],
+    showPersons: false
+  }
+
+  switchNameHandler = () => {
+    //console.log('was Clicked');
+    //Don't Try This this.state.persons[0].name='Mubashar Akram';
+    this.setState({
+      persons: [
+        { name: 'Mubashar Akram', age: 26 },
+        { name: 'Amir', age: 26 },
+        { name: 'Babar', age: 24 },
+        { name: 'Umar', age: 24 }
+      ]
+    })
+  }
+  toggleNameHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    })
+  }
+  deleteNameHandler = (index) => {
+    // const persons= this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(index, 1);
+    this.setState({
+      persons: persons
+    })
+  }
+  render() {
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+
+        <div className='center'>
+          {
+            this.state.persons.map((person, currentIndex) => {
+              return <Person
+                click={() => this.deleteNameHandler(currentIndex)}
+                name={person.name}
+                age={person.age} />
+            })
+          }
+        </div>
+      )
+    }
+    return (
+      <div className="App">
+        <h1>Hi Now I'm going to start my React Journey!</h1>
+        <button className='btn btn-blue' onClick={this.toggleNameHandler} >Switch Name</button>
+        {persons}
+      </div>
+    );
+  }
 }
 
 export default App;
